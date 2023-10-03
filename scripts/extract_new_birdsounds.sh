cca5ba842eea46dc34f2aa7d671b8d56107a94e8
Dr. Richard Zinck <cerambyx@mailbox.org>
2023-10-03 05:56:58 +0200

    Set conf to 0.9

--- scripts/extract_new_birdsounds.sh
+++ scripts/extract_new_birdsounds.sh
@@ -98,7 +98,7 @@ for h in "${SCAN_DIRS[@]}";do
     # context. If EXTRACTION_LENGTH is 10, for instance, 3 seconds are removed
     # from that value and divided by 2, so that the 3 seconds of the call are
     # within 3.5 seconds of audio context before and after.
-    [ -z ${EXTRACTION_LENGTH} ] && EXTRACTION_LENGTH=1.2
+    [ -z ${EXTRACTION_LENGTH} ] && EXTRACTION_LENGTH=1.25
     SPACER=$(echo "scale=4;(${EXTRACTION_LENGTH} - 0.5625 )/2" |bc -l)
     START=$(echo "scale=4;${START} - ${SPACER}"|bc -l)
     END=$(echo "scale=4;${END} + ${SPACER}"|bc -l)
