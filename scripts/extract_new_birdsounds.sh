1ed4f8fd5b310f90abd738b249bc7b6ea5f3879b
Dr. Richard Zinck <cerambyx@mailbox.org>
2023-10-03 03:48:35 +0200

    DEbug attempt

--- scripts/extract_new_birdsounds.sh
+++ scripts/extract_new_birdsounds.sh
@@ -115,6 +115,9 @@ for h in "${SCAN_DIRS[@]}";do
       END=${RECORDING_LENGTH}
     fi
 
+    echo "Start: " "${START}"
+    echo "End: " "${END}"
+
     sox -V1 "${h}/${OLDFILE}" "${NEWSPECIES_BYDATE}/${NEWFILE}" \
       trim ="${START}" ="${END}"
 
