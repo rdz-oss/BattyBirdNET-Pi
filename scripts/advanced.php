ee97a9fcd0dcd1e8c5406f4706748185850778d8
Dr. Richard Zinck <cerambyx@mailbox.org>
2023-10-04 12:33:04 +0200

    Adjust allowed values

--- scripts/advanced.php
+++ scripts/advanced.php
@@ -431,11 +431,11 @@ if (file_exists('./scripts/thisrun.txt')) {
       <input name="channels" type="number" min="1" max="32" step="1" value="<?php print($newconfig['CHANNELS']);?>" required/><br>
       <p>Set Channels to the number of channels supported by your sound card. 32 max.</p>
       <label for="recording_length">Recording Length: </label>
-      <input name="recording_length" oninput="document.getElementsByName('extraction_length')[0].setAttribute('max', this.value);" type="number" min="3" max="60" step="1" value="<?php print($newconfig['RECORDING_LENGTH']);?>" required/><br>
-      <p>Set Recording Length in seconds between 6 and 60. Multiples of 3 are recommended, as BirdNET analyzes in 3-second chunks.</p> 
+      <input name="recording_length" oninput="document.getElementsByName('extraction_length')[0].setAttribute('max', this.value);" type="number" min="0.5625" max="2.8125" step="0.5625" value="<?php print($newconfig['RECORDING_LENGTH']);?>" required/><br>
+      <p>Set Recording Length in seconds between 0.5625 and 2.8125. Multiples of 0.5625 are recommended, as BattyBirdNET analyzes in 0.5625-second chunks.</p>
       <label for="extraction_length">Extraction Length: </label>
-      <input name="extraction_length" oninput="this.setAttribute('max', document.getElementsByName('recording_length')[0].value);" type="number" min="0.6" value="<?php print($newconfig['EXTRACTION_LENGTH']);?>" /><br>
-      <p>Set Extraction Length to something less than your Recording Length. Min=3 Max=Recording Length</p>
+      <input name="extraction_length" oninput="this.setAttribute('max', document.getElementsByName('recording_length')[0].value);" type="number" min="0.5" value="<?php print($newconfig['EXTRACTION_LENGTH']);?>" /><br>
+      <p>Set Extraction Length to something less than your Recording Length. Min=0.5 Max=Recording Length</p>
       <label for="audiofmt">Extractions Audio Format</label>
       <select name="audiofmt">
       <option selected="<?php print($newconfig['AUDIOFMT']);?>"><?php print($newconfig['AUDIOFMT']);?></option>
