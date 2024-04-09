04cc3c8ff11598814b5b990a8a95a45e9b0e89f2
Dr. Richard Zinck <63958211+rdz-oss@users.noreply.github.com>
2024-04-09 17:39:33 +0200

    Test: remobe (?) unnecessary sleep from analysis

--- scripts/batnet_analysis.sh
+++ scripts/batnet_analysis.sh
@@ -9,7 +9,7 @@ start_service() {
   PYTHON_VIRTUAL_ENV="$HOME/BirdNET-Pi/birdnet/bin/python3"
   DIR="$HOME/BattyBirdNET-Analyzer"
   cd $DIR || return
-  sleep .5
+  # sleep .5
   if [ -z ${BAT_CLASSIFIER} ];then
     BAT_CLASSIFIER="Bavaria"
     echo "No classifier for bats set. Using default Bavaria classifier!"
