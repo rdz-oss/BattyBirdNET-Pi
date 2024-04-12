f1157e17e416ff7548601c12a8654443e71173ab
Dr. Richard Zinck <63958211+rdz-oss@users.noreply.github.com>
2024-04-12 22:36:12 +0200

    Input noise reduction with noisy visualization

--- scripts/birdnet_analysis.sh
+++ scripts/birdnet_analysis.sh
@@ -161,14 +161,14 @@ ${INCLUDEPARAM} \
 ${EXCLUDEPARAM} \
 ${BIRDWEATHER_ID_LOG}
 
-    if [[ $INPUT_NOISERED == true ]];then
-      sox "${1}/${i}" "${1}/${i}.out.wav" noisered "${HOME}/${NOISE_PROF}" ${NOISE_PROF_FACTOR} && mv "${1}/${i}.out.wav" "${1}/${i}"
-    fi
-
     #echo "${1}/${i}" > $HOME/BirdNET-Pi/analyzing_now.txt
     spectrogram_png=${EXTRACTED}/spectrogram.png
     analyzing_now="${1}/${i}"
     sox -V1 "${1}/${i}" -n remix 1 rate "${SAMPLING_RATE}" spectrogram -c "${analyzing_now//$HOME\//}" -o "${spectrogram_png}"
+
+    if [[ $INPUT_NOISERED == true ]];then
+      sox "${1}/${i}" "${1}/${i}.out.wav" noisered "${HOME}/${NOISE_PROF}" ${NOISE_PROF_FACTOR} && mv "${1}/${i}.out.wav" "${1}/${i}"
+    fi
 
     $PYTHON_VIRTUAL_ENV $DIR/analyze.py \
       --i "${1}/${i}" \
