3697dcf1033917700922d166b044ed6730d940aa
Pete-the-Geek <32913075+Pete-the-Geek@users.noreply.github.com>
2024-04-23 20:27:38 +0100

    Update advanced.php
    Integrated South Wales

--- scripts/advanced.php
+++ scripts/advanced.php
@@ -588,7 +588,7 @@ if (file_exists('./scripts/thisrun.txt')) {
       <select name="bat_classifier">
       <option selected="<?php print($newconfig['BAT_CLASSIFIER']);?>"><?php print($newconfig['BAT_CLASSIFIER']);?></option>
       <?php
-        $formats = array("Bavaria", "South Wales", "USA", "USA-EAST","USA-WEST","UK");
+        $formats = array("Bavaria", "South-Wales", "USA", "USA-EAST","USA-WEST","UK");
             foreach($formats as $format){
             echo "<option value='$format'>$format</option>";
         }
